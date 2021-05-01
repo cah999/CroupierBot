3db2de5c6814d6263b0d1d2936b49c83776765fb
cah999 <73055263+cah999@users.noreply.github.com>
2021-05-01 13:01:30 +0700

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -133,14 +133,13 @@ class mod(commands.Cog):
     async def on_member_join(self, member):
         role = discord.utils.get(member.guild.roles, name="Участник")
         await member.add_roles(role)
-        
+
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        if before.role != after.role:
-            if after.member.id == 483866841148686337:
-                new_role = after.role
-                if new_role.id in [774285935240151050, 774285953350369291]:
-                    after.member.remove(new_role)
+        if after.member.id == 483866841148686337:
+            new_role = after.role
+            if new_role.id in [774285935240151050, 774285953350369291]:
+                after.member.remove(new_role)
 
     # # Invite Create
     # @commands.Cog.listener()
